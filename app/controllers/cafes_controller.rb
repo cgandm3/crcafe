6beb0d2f813910fc67e5bc0ac8b327526649df64
Steve F Soter <cgandm3@me.com>
2015-07-07 16:54:42 -0700

    amazon api with keys working

---
+++ app/controllers/cafes_controller.rb
@@ -0,0 +1,31 @@
+class CafesController < ApplicationController
+
+  ## SHOWS ALL POSTS
+  def index
+
+    @keyword = params[:search]
+  ## VACUUM IS THE GEM USED FOR AMAZON SEARCH API IN RUBY
+      request = Vacuum.new('US')
+  ## AWS CONFIGURATION. ASSOCIATE_TAG IS A REFERRAL ID IN THE INSTANCE SOMEONE USES MY LINKS
+      request.configure(
+      aws_access_key_id: ENV['aws_access_key_id'],
+      aws_secret_access_key: ENV['aws_secret_access_key'],
+      associate_tag: ENV['associate_tag']
+      )
+
+      params = {
+        'SearchIndex' => 'All',
+        'Keywords'=> @keyword,
+        'ResponseGroup' => "ItemAttributes,Images"
+      }
+  ## DEFINES THE REQUEST RETURN
+      raw_products = request.item_search(query: params)
+      hashed_products = raw_products.to_h
+  ## PUTS SPECIFICS OF HASHED SEARCH RESPONSE INTO AN ARRAY
+      @products = hashed_products['ItemSearchResponse']['Items']['Item']
+
+  end
+  ## THIS ACTION CREATES A POST, ITEMS THAT ARE ASSOCIATED WITH THE POST, AND THE POSTITEM ENTRY.
+
+
+end
