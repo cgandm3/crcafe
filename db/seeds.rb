93974c7b52957af6b967baab39c187dad3871efe
Paul Aglione <aglionep@gmail.com>
2015-07-08 17:04:07 -0700

    seed 4 coffees

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-
-{description:
-  price:
-  image:}
+{description: "Cafe Britt Tarrazu Montecielo Ground Coffee, 12-Ounce Bags (Pack of 2)"
+  price: "17.90"
+  image_url: "http://ecx.images-amazon.com/images/I/51vh1KyvofL._SL160_.jpg"
+},
+{description: "Cafe Britt Costa Rica Organic Shade Grown Ground Coffee, 12-Ounce Bags (Pack of 2)"
+  price: "19.90"
+  image_url: "http://ecx.images-amazon.com/images/I/51S4tpxqTbL._SL160_.jpg"
+},
+{description: "Costa Rica Tarrazu, Whole Bean, Fresh Roasted Coffee LLC (2Lb.)"
+  price: "22.95"
+  image_url: "http://ecx.images-amazon.com/images/I/51fwv1kVV8L._SL160_.jpg"
+},
+{description: "Cafe Britt Costa Rica Organic Bajo Sombra Whole Bean Coffee, 12-Ounce Bags (Pack of 2)"
+  price: "19.90"
+  image_url: "http://ecx.images-amazon.com/images/I/51QQtF7cJ1L._SL160_.jpg"
+},
+{description: "Cafe Britt Costa Rica Organic Bajo Sombra Whole Bean Coffee, 12-Ounce Bags (Pack of 2)"
+  price: "19.90"
+  image_url: "http://ecx.images-amazon.com/images/I/41Q76Fd7eqL._SL160_.jpg"  
