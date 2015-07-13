d97a239ef2a654443ee73c3fd68a6fd54a401b28
Paul Aglione <aglionep@gmail.com>
2015-07-13 11:24:55 -0700

    render 90 coffees on index page

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -2,43 +2,45 @@ class CafesController < ApplicationController
 
   # SHOWS ALL POSTS
   def index
-    i = 1
-    while i < 11     
-    # @keyword = params[:search]
-  ## VACUUM IS THE GEM USED FOR AMAZON SEARCH API IN RUBY
-      request = Vacuum.new('US')
-  ## AWS CONFIGURATION. ASSOCIATE_TAG IS A REFERRAL ID IN THE INSTANCE SOMEONE USES MY LINKS
-      request.configure(
-      aws_access_key_id: ENV['aws_access_key_id'],
-      aws_secret_access_key: ENV['aws_secret_access_key'],
-      associate_tag: ENV['associate_tag']
-      )
+    @cafes = Cafe.all
 
-      params = {
-        'SearchIndex' => 'GourmetFood',
-        'ItemPage' => "2",
-        'Keywords'=> 'costa rican coffee',
-        'ResponseGroup' => "ItemAttributes,Images,Offers"
-      }
-  ## DEFINES THE REQUEST RETURN
-      raw_products = request.item_search(query: params)
-      hashed_products = raw_products.to_h
-      @products = hashed_products['ItemSearchResponse']['Items']['Item']
-  #     # Seed Datebase - One-Time Operation
-  #     @products.each do |product|
-  #     next unless product['Offers']['Offer']
+  #   i = 1
+  #   while i < 11     
+  #   # @keyword = params[:search]
+  # ## VACUUM IS THE GEM USED FOR AMAZON SEARCH API IN RUBY
+  #     request = Vacuum.new('US')
+  # ## AWS CONFIGURATION. ASSOCIATE_TAG IS A REFERRAL ID IN THE INSTANCE SOMEONE USES MY LINKS
+  #     request.configure(
+  #     aws_access_key_id: ENV['aws_access_key_id'],
+  #     aws_secret_access_key: ENV['aws_secret_access_key'],
+  #     associate_tag: ENV['associate_tag']
+  #     )
+
+  #     params = {
+  #       'SearchIndex' => 'GourmetFood',
+  #       'ItemPage' => "2",
+  #       'Keywords'=> 'costa rican coffee',
+  #       'ResponseGroup' => "ItemAttributes,Images,Offers"
+  #     }
+  # ## DEFINES THE REQUEST RETURN
+  #     raw_products = request.item_search(query: params)
+  #     hashed_products = raw_products.to_h
+  #     @products = hashed_products['ItemSearchResponse']['Items']['Item']
+  # #     # Seed Datebase - One-Time Operation
+  # #     @products.each do |product|
+  # #     next unless product['Offers']['Offer']
       
-  #     Cafe.create([{
-  #       mimg: product ['MediumImage']['URL'],
-  #        limg: product ['LargeImage']['URL'],  
-  #       desc: product['ItemAttributes']['Title'],
-  #       feature: product['ItemAttributes']['Feature'],
-  #       price: product['Offers']['Offer']['OfferListing']['Price']['Amount'],
-  #       fprice: product['Offers']['Offer']['OfferListing']['Price']['FormattedPrice']
-  #       }])
-  #       end
-        i += 1
-      end
+  # #     Cafe.create([{
+  # #       mimg: product ['MediumImage']['URL'],
+  # #        limg: product ['LargeImage']['URL'],  
+  # #       desc: product['ItemAttributes']['Title'],
+  # #       feature: product['ItemAttributes']['Feature'],
+  # #       price: product['Offers']['Offer']['OfferListing']['Price']['Amount'],
+  # #       fprice: product['Offers']['Offer']['OfferListing']['Price']['FormattedPrice']
+  # #       }])
+  # #       end
+  #       i += 1
+  #     end
     end
   ## THIS ACTION CREATES A POST, ITEMS THAT ARE ASSOCIATED WITH THE POST, AND THE POSTITEM ENTRY.
 end
