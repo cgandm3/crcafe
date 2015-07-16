5e35ac11c36c716643f1afbe479c514cde5d0cff
Paul Aglione <aglionep@gmail.com>
2015-07-16 14:30:24 -0700

    elastic search not working, 1 record in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,55 +7,58 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Seeding the DB using an API Call from Amazon
-  i = 1
-    while i < 11  
-    # @keyword = params[:search]
-  ## VACUUM IS THE GEM USED FOR AMAZON SEARCH API IN RUBY
-      request = Vacuum.new('US')
-  ## AWS CONFIGURATION. ASSOCIATE_TAG IS A REFERRAL ID IN THE INSTANCE SOMEONE USES MY LINKS
-      request.configure(
-      aws_access_key_id: ENV['aws_access_key_id'],
-      aws_secret_access_key: ENV['aws_secret_access_key'],
-      associate_tag: ENV['associate_tag']
-      )
+i = 1
+while i < 11  
+# @keyword = params[:search]
+## VACUUM IS THE GEM USED FOR AMAZON SEARCH API IN RUBY
+  request = Vacuum.new('US')
+## AWS CONFIGURATION. ASSOCIATE_TAG IS A REFERRAL ID IN THE INSTANCE SOMEONE USES MY LINKS
+  request.configure(
+    aws_access_key_id: ENV['aws_access_key_id'],
+    aws_secret_access_key: ENV['aws_secret_access_key'],
+    associate_tag: ENV['associate_tag']
+  )
 
-      params = {
-        'SearchIndex' => 'GourmetFood',
-        'ItemPage' => i,
-        'Keywords'=> 'costa rican coffee',
-        'ResponseGroup' => "ItemAttributes,Images,Offers"
-      }
-  ## DEFINES THE REQUEST RETURN
-      raw_products = request.item_search(query: params)
-      hashed_products = raw_products.to_h
-      @products = hashed_products['ItemSearchResponse']['Items']['Item']
-      # raise @products.inspect
-  #     # Seed Datebase - One-Time Operation
-      @products.each do |product|
-        
-        if product['Offers']['Offer'] != nil 
-          if product['MediumImage'] != nil
+  params = {
+    'SearchIndex' => 'GourmetFood',
+    'ItemPage' => i,
+    'Keywords'=> 'costa rican coffee',
+    'ResponseGroup' => "ItemAttributes,Images,Offers"
+  }
+## DEFINES THE REQUEST RETURN
+  raw_products = request.item_search(query: params)
+  hashed_products = raw_products.to_h
+  @products = hashed_products['ItemSearchResponse']['Items']['Item']
+  # raise @products.inspect
+#     # Seed Datebase - One-Time Operation
+  @products.each do |product|
+    
+    if product['Offers']['Offer'] != nil 
+      if product['MediumImage'] != nil
 
 
-            #next unless product['Offers']['Offer']
-            # next unless product['MediumImage']['URL'] != ""
+        #next unless product['Offers']['Offer']
+        # next unless product['MediumImage']['URL'] != ""
 
-            puts "----------------------------------"
-            puts product['ItemAttributes']['Title']
-            puts product['Offers']['Offer']
-            puts product['MediumImage']['URL']
-            puts "--------------------------------------------------------------------------------"
+        puts "----------------------------------"
+        puts product['ItemAttributes']['Title']
+        puts product['Offers']['Offer']
+        puts product['MediumImage']['URL']
+        puts "--------------------------------------------------------------------------------"
 
-            Cafe.create([{
-              mimg: product['MediumImage']['URL'],
-              limg: product['LargeImage']['URL'],  
-              desc: product['ItemAttributes']['Title'],
-              feature: product['ItemAttributes']['Feature'],
-              price: product['Offers']['Offer']['OfferListing']['Price']['Amount'],
-              fprice: product['Offers']['Offer']['OfferListing']['Price']['FormattedPrice']
-              }])
-          end
-        end
+        Cafe.create(
+          mimg: product['MediumImage']['URL'],
+          limg: product['LargeImage']['URL'],  
+          desc: product['ItemAttributes']['Title'],
+          feature: product['ItemAttributes']['Feature'],
+          price: product['Offers']['Offer']['OfferListing']['Price']['Amount'],
+          fprice: product['Offers']['Offer']['OfferListing']['Price']['FormattedPrice']
+        )
       end
-        i += 1
     end
+  end
+    i += 1
+end
+
+# Reindex for elasticsearch
+Cafe.reindex
