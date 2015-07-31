1ad47243d9d01072307d03d933e644ae7c1a9977
Paul Aglione <aglionep@gmail.com>
2015-07-31 15:55:59 -0700

    add home and shop links to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'page#home'
+
+  get 'cart/index'
+
   get 'sessions/new'
 
   get 'sessions/show'
@@ -18,7 +22,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'cafes#index'
+  # root 'cafes#index'
 
   # match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   # match 'auth/failure', to: redirect('/'), via: [:get, :post]
